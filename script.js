0bdd422e3bdf03982e50619a068d538a4f7d2605
Ariba-Jawaid721 <javeedsania124@gmail.com>
2026-06-06 01:26:11 +0500

    let,const,function,arrays

---
+++ script.js
@@ -0,0 +1,74 @@
+let age = 20;
+
+console.log(age);
+
+let city = 'karachi';
+
+city = 'lahore';
+
+console.log(city);
+
+const countryName = 'Pakistan';
+
+console.log(countryName);
+
+let marks = 90;
+
+const studentName = 'Ariba';
+
+console.log(marks);
+console.log(studentName);
+
+//Functions
+
+function greet(){
+ console.log("Hello world");}
+
+function showAge(age){
+    console.log(age);
+}
+
+function add(a , b){
+    return a + b;
+}
+ greet ();
+ showAge(20);
+
+ let result = add(10 , 5);
+ console.log(result);
+
+ function multiply(a, b){
+    return a * b};
+
+ let answer = multiply(10 , 2);
+ console.log(answer);
+ 
+//Array
+
+let students = ["Ariba", "Zainab", "Maida"];
+
+console.log(students);
+
+let fruits = ["Apple" , "Banana", "Grapes", "Mango"];
+
+console.log(fruits[3]);
+console.log(fruits.length);
+
+
+let cities = ["karachi" , "sukkur", "hyderabad"];
+cities.push("Islamabad");
+cities.pop();
+console.log(cities);
+
+let subjects = ["English", "Math", "Chemistry", "Biology"];
+for(i = 0; i < subjects.length; i++){
+    console.log(subjects[i]);
+    
+}
+
+
+
+
+ 
+
+
