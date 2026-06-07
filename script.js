9a4acbdba375863d7122d498c6a713ca7c2d9396
Ariba-Jawaid721 <javeedsania124@gmail.com>
2026-06-07 17:24:18 +0500

    loops and objects

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-let age = 20;
+//let age = 20;
 
-console.log(age);
+//console.log(age);
 
 let city = 'karachi';
 
@@ -12,12 +12,12 @@ const countryName = 'Pakistan';
 
 console.log(countryName);
 
-let marks = 90;
+//let marks = 90;
 
-const studentName = 'Ariba';
+//const studentName = 'Ariba';
 
-console.log(marks);
-console.log(studentName);
+//console.log(marks);
+//console.log(studentName);
 
 //Functions
 
@@ -45,9 +45,9 @@ function add(a , b){
  
 //Array
 
-let students = ["Ariba", "Zainab", "Maida"];
+//let students = ["Ariba", "Zainab", "Maida"];
 
-console.log(students);
+//console.log(students);
 
 let fruits = ["Apple" , "Banana", "Grapes", "Mango"];
 
@@ -63,12 +63,62 @@ console.log(cities);
 let subjects = ["English", "Math", "Chemistry", "Biology"];
 for(i = 0; i < subjects.length; i++){
     console.log(subjects[i]);
+    }
+
+//Conditional statement
+//prompt('Enter your age')
+let age = 18;
+if(age > 18){
+    console.log("you can vote");
+    
+} else {
+    console.log("you can't vote");
     
 }
 
+ let Eligibility = prompt("Enter your age for CNIC")
+let status = Eligibility >= 18 ? "Eligible" : "Not Eligible";
+console.log(status);
 
+//loops
+for(let i = 1; i <= 10; i++){
+console.log(i);
+}
+//table print 
 
-
+let num = 7;
+for(let i = 1; i <= 10; i++){
+ console.log(num * i);
+}
+//odd numbers print
+for(let i = 1; i <=20; i+=2){
+console.log(i);
+}
  
+let marks = [90, 40 , 75, 32];
+for(let i=0; i < marks.length; i++){
+ if(marks [i]<= 50){
+    console.log("Pass");
+ } else{
+    console.log("Fail");
+ }
+}
+//objects
+let student = {
+    name: "Ariba",
+    age : 22,
+    city : "karachi"
+};
+console.log(student.name);
 
-
+let students = [
+    {
+        name: "Ariba",
+        age : 22
+    },
+        {
+        name: "Waseem",
+        age: 35
+        }
+];
+console.log(students);
